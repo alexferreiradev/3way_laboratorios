7643878532d0e92959d56ee878c4be402ef426b9
Alex R. Ferreira <arf92@live.com>
2017-11-14 06:49:17 -0200

    add de codigos com To-do para lab 5 ex3

--- Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab05/ex3/TestaPassagemValor.java
+++ Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab05/ex3/TestaPassagemValor.java
@@ -9,6 +9,7 @@ public class TestaPassagemValor {
 		// Imprime valor de i
 		System.out.println("inicia método main e i = " + i);
 
+		//todo 5 - Observe que foi criada uma nova variavel do tipo primitivo, ela deve ser passada para o metodo test()
 		//cria variavel de teste como manda o exercicio
 		int teste = 10;
 		
@@ -18,28 +19,33 @@ public class TestaPassagemValor {
 		// e passa valor inteiro como um parâmetro. Uma vez que
 		// int é um tipo primitivo, este argumento e passado
 		// por valor.
-		test(i, teste);
+
+        // todo 5 - altera chamada para receber mais um parametro primitivo
+		test(i);
 
 		// imprime o valor de i, note que valor de i não muda
 		System.out.println("termina o método main e i = " + i);
 		
-		// imprime o valor de k, note que valor de k não muda
-		System.out.println("inicia método main e k = " + teste);
+		// todo 6 - Adiciona um sysout para mostrar o valor da variável "teste"
 	}
 
+    // todo 1 - altere o método para receber mais um parametro primitivo
 	// Método estático na classe
-	public static void test(int j, int k) {
+	public static void test(int j) {
 
 		System.out.println("inicia método test e j = " + j);
 
 		// muda valor parâmetro j
-		j = 33;
+		j = 33 + 3;
 		System.out.println("termina método test e j = " + j);
-		
-		System.out.println("inicia método test e k = " + k);
-		// muda valor parâmetro k
-		k = 20;
-		System.out.println("termina método test e k = " + k);
+
+		// todo 2 - adicione um sysout para mostrar o valor do parametro adicionado
+		System.out.println("inicia método test e parametro x = ");
+
+        // todo 3 - adiciona uma alteração no parametro adicionado, como soma, adição, divisão
+
+        // todo 4 - Mostra o novo valor do parametro adicionado
+		System.out.println("termina método test e parametro x = ");
 	}
 
 }
