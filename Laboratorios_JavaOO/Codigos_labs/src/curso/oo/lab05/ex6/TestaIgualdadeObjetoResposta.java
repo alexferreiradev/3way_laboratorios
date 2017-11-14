24efc6a7b884c1603adedbc286cec6cb16bc6bf8
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-11-14 13:55:25 -0200

    add todo ex6 lab5

--- Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab05/ex6/TestaIgualdadeObjetoResposta.java
+++ Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab05/ex6/TestaIgualdadeObjetoResposta.java
@@ -9,7 +9,8 @@ public class TestaIgualdadeObjetoResposta {
 
 		// Inicializando as variáveis. Note que as variáveis contem
 		str1 = "Viver sem Deus...não é viver!";
-		str2 = "Viver sem Deus...não é viver!";
+		// todo - altere str2 para conter o mesmo valor literal da str1 mas como nova instancia string
+		str2 = str1;
 
 		// Mostra o valor das variáveis str1 e str2
 		System.out.println("String1: " + str1);
@@ -21,6 +22,7 @@ public class TestaIgualdadeObjetoResposta {
 
 		// Checa se str1 e str2 são o mesmo objeto para as duas variáveis
 		// apontando para mesma instância de um objeto do tipo String
+		// todo observar o resultado
 		System.out.println("Mesmo objeto? " + ( str1 == str2 ));
 
 		// Checa se str1 e str2 tem o mesmo valor
--- Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab05/ex6/TesteIgualdadeIntegerResposta.java
+++ Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab05/ex6/TesteIgualdadeIntegerResposta.java
@@ -5,10 +5,12 @@ public class TesteIgualdadeIntegerResposta {
 	public static void main(String[] args) {
 
 		// Declara duas variáveis do tipo Integer, int1 e int2
+		// todo - crie vars Integer
 		Integer int1, int2;
 
 		// Inicializando as variáveis. Note que as variáveis contem
 		// ponteiros para instância de mesmo objeto atual.
+		// todo - observe a inicializacao
 		int1 = 10;
 		int2 = int1;
 
@@ -18,10 +20,12 @@ public class TesteIgualdadeIntegerResposta {
 
 		// Checa se int1 e int2 são o mesmo objeto para as duas variáveis
 		// apontando para mesma instância de um objeto do tipo Integer
-		System.out.println("Mesmo objeto? " + ( int1 == int2 ));
+		// todo - faça a comparação similar a string para inteiro agora
+		System.out.println("Mesmo objeto? " );
 
 		// Reinicializa variável int2. Ela agora aponta para um nova
 		// instância de objeto Integer
+		// todo - observe e leia o coment
 		int2 = new Integer(int1);
 
 		// Mostra o valor das variáveis int1 e int2
@@ -31,9 +35,11 @@ public class TesteIgualdadeIntegerResposta {
 		// Checa novamente se int1 e int2 são os mesmos
 		// objetos para as duas variáveis
 		// apontando para mesma instância de um objeto do tipo Integer
-		System.out.println("Mesmo objeto? " + ( int1 == int2 ));
+		// todo - faça a comparação similar a string para inteiro agora
+		System.out.println("Mesmo objeto? " );
 
 		// Checa se str1 e str2 tem o mesmo valor
+		// todo - faça a comparação com equals do Object
 		System.out.println("Mesmo valor? " + int1.equals(int2));
 
 	}
