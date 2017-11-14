16d6114a82ab45d588d031090e2c12351b2fefd9
Alex R. Ferreira <arf92@live.com>
2017-11-14 07:05:09 -0200

    add de to-do para ex3 final

--- Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab05/ex3/TestaPassagemReferencia.java
+++ Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab05/ex3/TestaPassagemReferencia.java
@@ -6,12 +6,14 @@ public class TestaPassagemReferencia {
 
 	public static void main(String[] args) {
 
+	    //todo 1 - observe a criacao do array, ele é de tipo primitivo
 		// criando um array de inteiros
 		int[] i = { 10, 20, 30 };
 
 		// Imprime valor de i
 		System.out.println("inicia metodo main e i = " + Arrays.toString(i));
 
+		// todo 2 - observe que é passado o array para o metodo
 		// Chama método test que está definido abaixo,
 		// e passa por referência o array de inteiro como um parâmetro.
 		// Uma vez que um array é do tipo referência,
@@ -19,6 +21,7 @@ public class TestaPassagemReferencia {
 		// por referência.
 		test(i);
 
+		// todo 4 - observer que o array foi alterado
 		// imprime o valor de i. Note que o valor de i não muda
 		System.out.println("termina o método main e i = " + Arrays.toString(i));
 	}
@@ -28,6 +31,7 @@ public class TestaPassagemReferencia {
 
 		System.out.println("inicia método test e j = " + Arrays.toString(j));
 
+		// todo 3 - observe que são alterados posicoes do array
 		// muda valor do parâmetro i
 		j[0] = 33;
 		j[1] = 66;
