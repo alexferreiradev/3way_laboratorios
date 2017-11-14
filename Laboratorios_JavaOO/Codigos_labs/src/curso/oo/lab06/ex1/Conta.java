d56b8f2c521c915aafb0cb6fc57ae54b8fa0e09a
javaterqui <javaterqui@suporte>
2017-11-14 20:14:46 -0200

    add todo para lab6 ex1

--- Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab06/ex1/Conta.java
+++ Laboratorios_JavaOO/Codigos_labs/src/curso/oo/lab06/ex1/Conta.java
@@ -19,44 +19,6 @@ public class Conta {
 		System.out.println("Data de abertura da Conta e : " + this.dataAbertura.toString());
 	}
 	
-	public int getNumero() {
-
-		return numero;
-	}
-
-	public void setNumero(int numero) {
-
-		this.numero = numero;
-	}
-
-	public String getTitular() {
-
-		return titular;
-	}
-
-	public void setTitular(String titular) {
-
-		this.titular = titular;
-	}
-
-	public double getSaldo() {
-
-		return saldo;
-	}
-
-	public void setSaldo(double saldo) {
-
-		this.saldo = saldo;
-	}
-
-	public Date getDataAbertura() {
-
-		return dataAbertura;
-	}
-
-	public void setDataAbertura(Date dataAbertura) {
-
-		this.dataAbertura = dataAbertura;
-	}
+	// todo - criar 
 
 }
